85a17668c9baaf77b9775a760b34914ba88c36cc
Natsuki Hasegawa <oosacker@gmail.com>
2021-04-06 21:46:03 +1200

    test

--- test.go
+++ test.go
@@ -72,6 +72,47 @@ func Sqrt(x float64) (z float64) {
 	return
 }
 
+func waiting() {
+	defer fmt.Println("waiting......")
+
+	fmt.Println("a")
+
+	fmt.Println("b")
+
+	fmt.Println("c")
+
+}
+
+func pointers() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(p)  // address of i
+	fmt.Println(*p) // value of i
+
+	*p = 21        // set i through the pointer
+	fmt.Println(i) // see the new value of i
+
+	p = &j         // point to j
+	fmt.Println(p) // address of j
+	*p = *p / 37   // divide j through the pointer
+
+	fmt.Println(j) // see the new value of j
+}
+
+type myStruct struct {
+	X int
+	Y int
+	z int
+}
+
+func (m myStruct) add() int {
+	return m.X + m.Y + m.z
+}
+
 func main() {
-	fmt.Println(Sqrt(2))
+	test := myStruct{1, 2, 6}
+
+	fmt.Println(test.add())
+
 }
